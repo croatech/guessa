53dd446c7ffcd4cf7f7fa5f2cb3eae3a22985deb
Vitaly <croaton6@gmail.com>
2015-04-24 04:09:28 +0300

    pg driver

--- app.go
+++ app.go
@@ -1,41 +1,51 @@
 package main
 
 import (
-    "fmt"
-    "html/template"
-    "github.com/julienschmidt/httprouter"
-    "net/http"
-    "log"
+	"database/sql"
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	_ "github.com/lib/pq"
+	"html/template"
+	"log"
+	"net/http"
 )
 
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-    t,_ := template.ParseFiles("views/index.html")
-    t.Execute(w, nil)
+	t, _ := template.ParseFiles("views/index.html")
+	t.Execute(w, nil)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-    fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
 }
 
 func Login(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-    fmt.Println("method:", r.Method) // get request method
-    if r.Method == "GET" {
-        t,_ := template.ParseFiles("views/login.html")
-        t.Execute(w, nil)
-    } else {
-        r.ParseForm()
-        fmt.Println("username:", r.Form["username"])
-        fmt.Println("password:", r.Form["password"])
-    }
+	fmt.Println("method:", r.Method) // get request method
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("views/login.html")
+		t.Execute(w, nil)
+	} else {
+		r.ParseForm()
+		fmt.Println("username:", r.Form["username"])
+		fmt.Println("password:", r.Form["password"])
+	}
 }
 
 func main() {
-    router := httprouter.New()
-    router.GET("/", Index)
-    router.GET("/hello/:name", Hello)
-    router.GET("/login", Login)
-    router.POST("/login", Login)
+	db, err := sql.Open("postgres", "user=pqgotest dbname=pqgotest sslmode=verify-full")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    log.Fatal(http.ListenAndServe(":3001", router))
-    log.Println("Listening...")
+	age := 21
+	rows, err := db.Query("SELECT name FROM users WHERE age = $1", age)
+
+	router := httprouter.New()
+	router.GET("/", Index)
+	router.GET("/hello/:name", Hello)
+	router.GET("/login", Login)
+	router.POST("/login", Login)
+
+	log.Fatal(http.ListenAndServe(":3001", router))
+	log.Println("Listening...")
 }
