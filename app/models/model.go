c96e211d8b10567abfb5c04db37e192a43de3fe5
Vitaly <croaton6@gmail.com>
2015-04-25 00:02:58 +0300

    del gorm

--- app/models/model.go
+++ app/models/model.go
@@ -1,10 +1,5 @@
 package models
 
-import (
-	"github.com/jinzhu/gorm"
-	_ "github.com/lib/pq"
-)
-
 type Model struct {
 	ID        uint `gorm:"primary_key"`
 	CreatedAt time.Time
--- app/models/user.go
+++ app/models/user.go
@@ -1,10 +1,5 @@
 package models
 
-import (
-	"github.com/jinzhu/gorm"
-	_ "github.com/lib/pq"
-)
-
 type User struct {
 	gorm.Model
 	Name string
