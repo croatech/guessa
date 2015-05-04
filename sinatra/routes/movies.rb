9451f91a95d46a88e716531ae9752ffbd0b7d127
Vitaly <croaton6@gmail.com>
2015-05-05 00:02:50 +0300

    implement game functionality

--- sinatra/routes/movies.rb
+++ sinatra/routes/movies.rb
@@ -1,10 +1,10 @@
 class GuessApp < Sinatra::Base
   namespace '/movies' do
-    @title = "Movies"
+    @title = 'Movies'
 
     get '' do
       if !session['user_name']
-        redirect "/"
+        redirect '/'
       end
 
       haml :'movies/index', layout: :layout
@@ -12,36 +12,43 @@ class GuessApp < Sinatra::Base
 
     get '/game' do
       if !session['user_name']
-        redirect "/"
+        redirect '/'
       end
 
-      if session['game'] != "started" # if the game hasn't started
+      if session['game'] != 'started' && session['game'] != 'finished' # if the game hasn't been started
         game = Game.create!(user_name: session['user_name'])
-        session['game'] = "started" # the game has started
+        session['game'] = 'started' # the game has been started
         session['game_id'] = game.id
       end
 
       @game = Game.find(session['game_id']) # current game
-      @movie = Movie.order("RANDOM()").first() 
-      @options = Movie.where.not(id: @movie.id).order("RANDOM()").first(3).to_a << @movie
+      @movie = Movie.order("RANDOM()").first() # movie for game
+      @movies = Movie.where.not(id: @movie.id).order("RANDOM()").first(3).to_a << @movie # other options + current movie
 
       haml :'movies/game', layout: :layout
     end
 
-    post '/game' do
+    post '/game' do # check for correct answer
       if !session['user_name']
-        redirect "/"
+        redirect '/'
       end
 
       @game = Game.find(session['game_id'])
 
       if params[:id] == params[:answer_id]
-        @game.right_answer
+        @game.right_answer # increment score
         redirect '/movies/game'
-        flash[:success] = "Правильный ответ!"
+        flash[:success] = 'Правильный ответ!'
       else
-        flash[:error] = "Неправильный ответ!"
+        session['game'] = 'finished' # the game has been finished
+        redirect '/movies/game'
+        flash[:error] = 'Неправильный ответ!'
       end
+    end
+
+    get '/game/next_one' do
+      session['game'] = 'prepared'
+      redirect 'movies/game'
     end
   end
 end
