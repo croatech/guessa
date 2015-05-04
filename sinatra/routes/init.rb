4e6ade34246e8d54550bb5234ff1bd789aa5e31a
Vitaly <croaton6@gmail.com>
2015-05-04 19:24:25 +0300

    added views

--- sinatra/routes/init.rb
+++ sinatra/routes/init.rb
@@ -3,6 +3,14 @@ require_relative "stars"
 
 class GuessApp < Sinatra::Base
   get '/' do
-    "Hello World"
+    haml :index, layout: :layout
+  end
+
+  get '/choose' do
+  	haml :choose, layout: :layout
+  end
+
+  post '/choose' do
+  	redirect "/choose"
   end
 end
