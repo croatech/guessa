145a16a0be07f8d60c392a763d5976fd0451c590
Vitaliy <croaton6@gmail.com>
2015-07-08 05:35:08 +0300

    fixed rating button toggle

--- app/assets/javascripts/angular/controllers/game.js
+++ app/assets/javascripts/angular/controllers/game.js
@@ -7,23 +7,25 @@ app.controller('GameCtrl', function($scope, Restangular, Games) {
   $scope.secondHelpOption = true
   $scope.thirdHelpOption = true
 
+  $scope.ratingButton = true
+
   $scope.startGame = function(userId) {
     $scope.score = 0
-    $scope.gameStatus = "processing";
+    $scope.gameStatus = "processing"
 
-    Games.create(userId);
+    Games.create(userId)
 
     Restangular.one('users', userId).all('games').getList().then(function(current_game) {
-      $scope.current_game = current_game.sort().reverse()[0];
-    });
-  };
+      $scope.current_game = current_game.sort().reverse()[0]
+    })
+  }
 
   $scope.getMovies = function() {
     Restangular.all('movies').getList().then(function(movies) {
       $scope.secret = movies[0]
       $scope.movies = shuffle(movies)
-    });
-  };
+    })
+  }
 
   $scope.checkAnswer = function(answerId, $index) {
     if ($scope.secret.id == answerId && $scope.gameStatus != "finished") {
@@ -38,15 +40,21 @@ app.controller('GameCtrl', function($scope, Restangular, Games) {
     } else {
       $scope.gameStatus = "finished"
       $scope.wrongAnswer = $index // set css style for wrong answer
-      $scope.secretMovieTitle = $scope.secret.title;
+      $scope.secretMovieTitle = $scope.secret.title
       removeBorder()
-    };
-  };
+    }
+  }
 
   $scope.showRating = function() {
-    $scope.ratingShows = true;
-    $scope.games = Games.getGames();
-  };
+    $scope.ratingShows = true
+    $scope.ratingButton = false
+    $scope.games = Games.getGames()
+  }
+
+  $scope.hideRating = function() {
+    $scope.ratingShows = false
+    $scope.ratingButton = true
+  }
 
   $scope.getFiftyFifty = function() {
     $scope.firstHelpOption = false
@@ -57,13 +65,13 @@ app.controller('GameCtrl', function($scope, Restangular, Games) {
       } else {
         false
       }
-    });
+    })
 
     $scope.movies.splice($indexSecretMovie, 1) // delete secret movie from an array
     $scope.movies.unshift($scope.secret)       // put secret movie to an array on first place  
     $scope.movies = $scope.movies.slice(0, 2)  // deletes 2 last options(secret movie is first)
     $scope.movies = shuffle($scope.movies)     // shuffle
-  };
+  }
 
   $scope.getSkipQuestion = function() {
     $scope.secondHelpOption = false
@@ -79,7 +87,7 @@ app.controller('GameCtrl', function($scope, Restangular, Games) {
     $scope.score = 0
     $scope.gameStatus = "finished"
     $scope.showRating()
-    $scope.secret.image = "http://www.tnca.org/wp-content/uploads/2012/02/helloloser.jpg";
+    $scope.secret.image = "http://www.tnca.org/wp-content/uploads/2012/02/helloloser.jpg"
   }
 
-});
+})
--- app/assets/javascripts/angular/services/game.js
+++ app/assets/javascripts/angular/services/game.js
@@ -2,14 +2,14 @@ app.factory('Games', function(Restangular) {
 
   return {
     create: function(userId) {
-      return Restangular.all('games').post({user_id: userId});
+      return Restangular.all('games').post({user_id: userId})
     },
     getMovies: function() {
-      return Restangular.all('movies').getList().$object;  // GET: /users
+      return Restangular.all('movies').getList().$object
     }, 
     getGames: function() {
-      return Restangular.all('games').getList().$object;
+      return Restangular.all('games').getList().$object
     }
-  };
+  }
 
-});
+})
