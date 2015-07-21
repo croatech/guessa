2ec2a06d20225de1d51402198f991a49e93c6f11
Vitaly <croaton6@gmail.com>
2015-07-22 01:42:00 +0300

    test allow origin

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -1,7 +1,8 @@
 var app = angular.module('Guessa', ['restangular'])
   .config(function (RestangularProvider) {
 
-   RestangularProvider.setBaseUrl("http://guessmovie.herokuapp.com");
+   //RestangularProvider.setBaseUrl("http://localhost:3000");
+   //RestangularProvider.setBaseUrl("http://guessmovie.herokuapp.com");
    RestangularProvider.setRequestSuffix('.json');
    RestangularProvider.setDefaultHttpFields({xsrfCookieName:'csrftoken', xsrfHeaderName:'X-CSRFToken'});
 });
