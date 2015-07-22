be0f66a2f10c8c7931cd0e619481b87745dd9ba3
Vitaly <croaton6@gmail.com>
2015-07-22 18:08:25 +0300

    fixed assets

--- public/assets/application-b875a1da150f9e8239185a83a1151dbeb5669a8c1567284f31125f544ec84ba1.js
+++ public/assets/application-b875a1da150f9e8239185a83a1151dbeb5669a8c1567284f31125f544ec84ba1.js
@@ -40371,9 +40371,12 @@ var minlengthDirective = function() {
 var app = angular.module('Guessa', ['restangular'])
   .config(function (RestangularProvider) {
 
-   RestangularProvider.setBaseUrl("http://localhost:3000");
+   //RestangularProvider.setBaseUrl("http://localhost:3000");
+   //RestangularProvider.setBaseUrl("http://guessmovie.herokuapp.com");
    RestangularProvider.setRequestSuffix('.json');
+   RestangularProvider.setDefaultHttpFields({xsrfCookieName:'csrftoken', xsrfHeaderName:'X-CSRFToken'});
 });
+
 app.controller('GameCtrl', function($scope, Restangular, Games) {
 
   // init
@@ -52681,6 +52684,7 @@ return jQuery;
   }
 
 })( jQuery );
+
 
 
 
