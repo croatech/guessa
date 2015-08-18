b6a35e032e5c084d29507d17132ea0fc07ec6293
Vitaly F <Frylock13@users.noreply.github.com>
2015-08-18 12:33:55 +0300

    Update movies_controller.rb

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
   def index
-  	random_year = Movie.all.order("RANDOM()").first().year
+    random_year = Movie.all.order("RANDOM()").first().year
     @movies = Movie.all.order("RANDOM()").where(year: random_year).limit(4)
   end
 end
